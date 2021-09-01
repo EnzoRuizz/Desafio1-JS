a8991b2e49c9fd844d48ed7ba5660a7001c66e53
EnzoRuizz <renzomach56@gmail.com>
2021-09-01 16:15:42 -0300

    Puse a console.log como comentario en caso de que se lo quiera usar en vez de a alert

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ var claseApellido = prompt("¿Cual es tu apellido?");
 var Año = prompt("¿En que año naciste?");
 var claseEdad = 2021 - Año;
 alert("Hola!"+" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
-console.log("Hola!"+" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
+/* console.log("Hola!"+" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años"); */
