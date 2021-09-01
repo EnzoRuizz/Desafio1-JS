3439079407893ff8d54c23ca1ecbebc67fe09d2f
EnzoRuizz <renzomach56@gmail.com>
2021-09-01 16:03:38 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var claseNombre = prompt("¿Cual es tu nombre?")
+var claseApellido = prompt("¿Cual es tu apellido?");
+var Año = prompt("¿En que año naciste?");
+var claseEdad = 2021 - Año;
+alert(" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
+console.log(" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
