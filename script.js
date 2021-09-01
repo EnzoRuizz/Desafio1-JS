ec3a1e041bb2d1366469ba451698b20641fa17c8
EnzoRuizz <renzomach56@gmail.com>
2021-09-01 16:12:01 -0300

    Cambios en alert y console

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var claseNombre = prompt("¿Cual es tu nombre?")
 var claseApellido = prompt("¿Cual es tu apellido?");
 var Año = prompt("¿En que año naciste?");
 var claseEdad = 2021 - Año;
-alert(" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
-console.log(" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
+alert("Hola!"+" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
+console.log("Hola!"+" "+ claseNombre +" "+ claseApellido +" tiene "+ claseEdad +" Años");
